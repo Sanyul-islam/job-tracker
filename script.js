a870e5f5b4d64ab1367a821ff78e5dec4d758b87
Sanyul-islam <imsanyulislam@gmail.com>
2026-02-23 03:06:06 +0600

    added interview toggle funtionality

--- script.js
+++ script.js
@@ -4,35 +4,120 @@ const allJobs = cardContainer.childElementCount;
 document.getElementById("totall-jobs").innerText = `${allJobs}`;
 document.getElementById("jobs-count").innerText = `${allJobs}`;
 if (allJobs == 0) {
-    let cardHidden = document.getElementById("card-hidden")
-    cardHidden.classList.remove("hidden")
+  let cardHidden = document.getElementById("card-hidden");
+  cardHidden.classList.remove("hidden");
 }
 
 // interview & rejected count
-let interviewList = [] ;
-let rejectedList = [] ;
+let interviewList = [];
+let rejectedList = [];
 
-interviewList.push ({name: 'sany'})
 const interviewCount = document.getElementById("totall-interview");
 const rejectedCount = document.getElementById("totall-rejected");
-calculateCount();
-function calculateCount () {
-    interviewCount.innerText = interviewList.length
-    rejectedCount.innerText = rejectedList.length
+const filterSection = document.getElementById("filter-section");
+function calculateCount() {
+  interviewCount.innerText = interviewList.length;
+  rejectedCount.innerText = rejectedList.length;
 }
+calculateCount();
 
 // toggle btn
+let currentStatus = 'all'
 const filterbtnAll = document.getElementById("filter-btn-all");
 const filterbtnInterview = document.getElementById("filter-btn-interview");
 const filterbtnRejected = document.getElementById("filter-btn-rejected");
 
-function toggleStyle(id){
-    filterbtnAll.classList.remove('btn-primary')
-    filterbtnInterview.classList.remove('btn-primary')
-    filterbtnRejected.classList.remove('btn-primary')
+function toggleStyle(id) {
+  filterbtnAll.classList.remove("btn-primary");
+  filterbtnInterview.classList.remove("btn-primary");
+  filterbtnRejected.classList.remove("btn-primary");
 
-    const selected = document.getElementById(id)
-    selected.classList.add('btn-primary')
+  const selected = document.getElementById(id);
+  selected.classList.add("btn-primary");
 
+  if (id == "filter-btn-interview") {
+    cardContainer.classList.add("hidden");
+    filterSection.classList.remove("hidden");
+  } else if (id == "filter-btn-all") {
+    cardContainer.classList.remove("hidden");
+    filterSection.classList.add("hidden");
+  }
 }
 
+// event delegation
+
+cardContainer.addEventListener("click", function (event) {
+  console.log(event.target.classList.contains("interview-btn"));
+  if (event.target.classList.contains("interview-btn")) {
+    const parent = event.target.parentNode.parentNode;
+    const companyName = parent.querySelector(".company-name").innerText;
+    const position = parent.querySelector(".position").innerText;
+    const locationTypeSalary = parent.querySelector(".location-type-salary").innerText;
+    let status = parent.querySelector(".Status").innerText;
+    const description = parent.querySelector(".description").innerText;
+    parent.querySelector(".Status").innerText = "Interview";
+    parent.querySelector(".Status").className = "btn btn-outline btn-success";
+    console.log(status)
+    const cardInfo = {
+      companyName,
+      position,
+      locationTypeSalary,
+      status: 'Interview',
+      description
+    };
+
+    const companyExis = interviewList.find(
+      (item) => item.companyName == cardInfo.companyName,
+    );
+
+    if (!companyExis) {
+      interviewList.push(cardInfo);
+    }
+    renderInterview();
+    calculateCount();
+}
+});
+
+function renderInterview() {
+  filterSection.innerHTML = ``;
+  for (let interview of interviewList) {
+    console.log(interview);
+    let div = document.createElement(`div`);
+    div.className = "w-11/12 container mx-auto space-y-5 py-10";
+    div.innerHTML = `<div class="bg-base-100 shadow-md py-6 px-5 space-y-5">
+          <div class="flex justify-between">
+            <div class="">
+              <h2 class="company-name text-[20px] font-bold">
+                ${interview.companyName}
+              </h2>
+              <p class="position">${interview.position}</p>
+            </div>
+            <div class="">
+              <button class="delet-btn cursor-pointer">
+                <img src="./asset/delet.png" alt="" />
+              </button>
+            </div>
+          </div>
+          <p class="location-type-salary">
+            ${interview.locationTypeSalary}
+          </p>
+          <div class="">
+            <button class="Status btn btn-outline btn-success">${interview.status}</button>
+            <p class="description cpt-2">
+             ${interview.description}
+            </p>
+          </div>
+          <div class="flex space-x-4">
+            <button class="interview-btn btn btn-outline btn-success">
+              INTERVIEW
+            </button>
+            <button class="rejected-btn btn btn-outline btn-error">
+              REJECTED
+            </button>
+          </div>
+        </div>
+          
+          `;
+    filterSection.appendChild(div);
+  }
+}
