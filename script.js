08cbdb80cf27871568d6c86b7200c1455be6c7a7
Sanyul-islam <imsanyulislam@gmail.com>
2026-02-23 21:14:45 +0600

    added rejected filter section

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 // All jobs count secion
 const cardContainer = document.getElementById("card-container");
-const allJobs = cardContainer.childElementCount;
+const allJobs = cardContainer.children.length;
+console.log(allJobs);
 document.getElementById("totall-jobs").innerText = `${allJobs}`;
 document.getElementById("jobs-count").innerText = `${allJobs}`;
-if (allJobs == 0) {
-  let cardHidden = document.getElementById("card-hidden");
-  cardHidden.classList.remove("hidden");
-}
 
 // interview & rejected count
 let interviewList = [];
@@ -15,6 +12,7 @@ let rejectedList = [];
 const interviewCount = document.getElementById("totall-interview");
 const rejectedCount = document.getElementById("totall-rejected");
 const filterSection = document.getElementById("filter-section");
+const cardHidden = document.getElementById("card-hidden");
 function calculateCount() {
   interviewCount.innerText = interviewList.length;
   rejectedCount.innerText = rejectedList.length;
@@ -22,7 +20,7 @@ function calculateCount() {
 calculateCount();
 
 // toggle btn
-let currentStatus = 'all'
+let currentStatus = "all";
 const filterbtnAll = document.getElementById("filter-btn-all");
 const filterbtnInterview = document.getElementById("filter-btn-interview");
 const filterbtnRejected = document.getElementById("filter-btn-rejected");
@@ -38,9 +36,24 @@ function toggleStyle(id) {
   if (id == "filter-btn-interview") {
     cardContainer.classList.add("hidden");
     filterSection.classList.remove("hidden");
+    // cardHidden.classList.remove("hidden")
   } else if (id == "filter-btn-all") {
     cardContainer.classList.remove("hidden");
     filterSection.classList.add("hidden");
+    // cardHidden.classList.add("hidden");
+  } else if (id == "filter-btn-rejected") {
+    cardContainer.classList.add("hidden");
+    filterSection.classList.remove("hidden");
+    //  cardHidden.classList.remove("hidden");
+  }
+  
+  if (allJobs == 0) {
+    cardHidden.classList.remove("hidden");
+    cardContainer.classList.add("hidden");
+  } else if (interviewList.length == 0) {
+    cardHidden.classList.remove("hidden");
+  } else if (rejectedList.length == 0) {
+    cardHidden.classList.remove("hidden");
   }
 }
 
@@ -52,18 +65,22 @@ cardContainer.addEventListener("click", function (event) {
     const parent = event.target.parentNode.parentNode;
     const companyName = parent.querySelector(".company-name").innerText;
     const position = parent.querySelector(".position").innerText;
-    const locationTypeSalary = parent.querySelector(".location-type-salary").innerText;
+    const locationTypeSalary = parent.querySelector(
+      ".location-type-salary",
+    ).innerText;
     let status = parent.querySelector(".Status").innerText;
     const description = parent.querySelector(".description").innerText;
     parent.querySelector(".Status").innerText = "Interview";
-    parent.querySelector(".Status").className = "btn btn-outline btn-success";
-    console.log(status)
+    parent
+      .querySelector(".Status")
+      .classList.add("btn btn-outline btn-success");
+    console.log(status);
     const cardInfo = {
       companyName,
       position,
       locationTypeSalary,
-      status: 'Interview',
-      description
+      status: "Interview",
+      description,
     };
 
     const companyExis = interviewList.find(
@@ -73,9 +90,40 @@ cardContainer.addEventListener("click", function (event) {
     if (!companyExis) {
       interviewList.push(cardInfo);
     }
+    if (interviewList.length == 0) {
+    }
     renderInterview();
     calculateCount();
-}
+  } else if (event.target.classList.contains("rejected-btn")) {
+    const parent = event.target.parentNode.parentNode;
+    const companyName = parent.querySelector(".company-name").innerText;
+    const position = parent.querySelector(".position").innerText;
+    const locationTypeSalary = parent.querySelector(
+      ".location-type-salary",
+    ).innerText;
+    // let status = parent.querySelector(".Status").innerText;
+    const description = parent.querySelector(".description").innerText;
+    parent.querySelector(".Status").innerText = "Rejected";
+    parent.querySelector(".Status").className = "btn btn-outline btn-error";
+    // console.log(status);
+    const cardInfo = {
+      companyName,
+      position,
+      locationTypeSalary,
+      status: "Rejected",
+      description,
+    };
+
+    const companyExis = rejectedList.find(
+      (item) => item.companyName == cardInfo.companyName,
+    );
+
+    if (!companyExis) {
+      rejectedList.push(cardInfo);
+    }
+    renderRejected();
+    calculateCount();
+  }
 });
 
 function renderInterview() {
@@ -105,6 +153,49 @@ function renderInterview() {
             <button class="Status btn btn-outline btn-success">${interview.status}</button>
             <p class="description cpt-2">
              ${interview.description}
+            </p>
+          </div>
+          <div class="flex space-x-4">
+            <button class="interview-btn btn btn-outline btn-success">
+              INTERVIEW
+            </button>
+            <button class="rejected-btn btn btn-outline btn-error">
+              REJECTED
+            </button>
+          </div>
+        </div>
+          
+          `;
+    filterSection.appendChild(div);
+  }
+}
+function renderRejected() {
+  filterSection.innerHTML = ``;
+  for (let rejected of rejectedList) {
+    console.log(rejected);
+    let div = document.createElement(`div`);
+    div.className = "w-11/12 container mx-auto space-y-5 py-10";
+    div.innerHTML = `<div class="bg-base-100 shadow-md py-6 px-5 space-y-5">
+          <div class="flex justify-between">
+            <div class="">
+              <h2 class="company-name text-[20px] font-bold">
+                ${rejected.companyName}
+              </h2>
+              <p class="position">${rejected.position}</p>
+            </div>
+            <div class="">
+              <button class="delet-btn cursor-pointer">
+                <img src="./asset/delet.png" alt="" />
+              </button>
+            </div>
+          </div>
+          <p class="location-type-salary">
+            ${rejected.locationTypeSalary}
+          </p>
+          <div class="">
+            <button class="Status btn btn-outline btn-error">${rejected.status}</button>
+            <p class="description cpt-2">
+             ${rejected.description}
             </p>
           </div>
           <div class="flex space-x-4">
